829258c13089d4e21daf2cb157cf1259117350b7
Nikita-numberOne <jasonzhora@mail.ru>
2024-08-02 15:24:49 +0300

    Add files via upload

---
+++ task.4.py
@@ -0,0 +1,5 @@
+task = 12
+work_time = 1.5
+Name_Course = "Python,"
+time_one_task = work_time/task
+print("Курс:", Name_Course,"всего задач:",task,",затрачено часов:",work_time,",среднее время выполнения:",time_one_task)
